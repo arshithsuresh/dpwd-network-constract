f24e3f46522725804c52fac23c2a295449ec5d22
Arshith Suresh <arshith007@gmail.com>
2022-05-26 01:15:58 +0530

    Complaint API - BASE DONE

--- server/nodejs/api/channel/road/complaint/ComplaintAPI.js
+++ server/nodejs/api/channel/road/complaint/ComplaintAPI.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const req = require('express/lib/request');
 const app = express.Router();
 
 app.get("/all",(req,res,next)=>{
-    res.send("all Complaints");
-})
+    
+});
+
+app.get("/:complaintID", (req,res,next)=>{
+
+});
+
+app.post("/create/", (req,res,next)=>{
+
+});
+
+app.patch("/:complaintID/sign", (req,res,next)=>{
+
+});
+
+app.patch("/:complaintID/upvote", (req,res,next)=>{
+
+});
+
+app.patch("/:complaintID/status", (req,res,next)=>{
+
+});
 
 
 
---
+++ server/nodejs/blockchain/complaint/Complaint.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const CONSTANTS = require('../../constants/constants');
+const GetChannel = require('../channel/channel')
+const res = require('express/lib/response');
+
+const GetAllComplaints = async (userid=CONSTANTS.ORG_ADMIN)=>{
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('getAllComplaints');
+    console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+
+    return result.toString();
+}
+
+const GetProjectByID = async (complaintID,userid=CONSTANTS.ORG_ADMIN)=>{
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('readComplaint',complaintID);    
+    
+    return result.toString();    
+}
+
+const SignProject = async(complaintID,userid=null)=>{
+
+    if(userid==null || complaintID == null)
+    {
+        return false;
+    }
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('signComplaint',complaintID);
+
+    return result.toString();
+
+}
+
+const VoteComplaint = async(complaintID,userid=null)=>{
+
+    if(userid==null || complaintID == null)
+    {
+        return false;
+    }
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('voteComplaint',complaintID,userid);
+
+    return result.toString();
+
+} 
+
+const FlagComplaintPending = async(complaintID,userid=null)=>{
+
+    if(userid==null || complaintID == null)
+    {
+        return false;
+    }
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('flagComplaintPending',complaintID);
+
+    return result.toString();
+}
+
+const FlagComplaintVerified = async(complaintID,userid=null)=>{
+
+    if(userid==null || complaintID == null)
+    {
+        return false;
+    }
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('flagComplaintVerified',complaintID);
+
+    return result.toString();
+}
+
+const FlagComplaintResolved = async(complaintID,userid=null)=>{
+
+    if(userid==null || complaintID == null)
+    {
+        return false;
+    }
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('flagComplaintResolved',complaintID);
+
+    return result.toString();
+}
+
+const FlagComplaintInvalid = async(complaintID,userid=null)=>{
+
+    if(userid==null || complaintID == null)
+    {
+        return false;
+    }
+
+    const network = await GetChannel.GetRoadChannel(userid)
+    const contract = network.getContract('Complaint-Contract');
+    const result = await contract.evaluateTransaction('flagComplaintInvalid',complaintID);
+
+    return result.toString();
+}
+
+module.exports = {GetAllComplaints, GetProjectByID, SignProject, VoteComplaint,
+    FlagComplaintPending, FlagComplaintVerified, FlagComplaintResolved, FlagComplaintInvalid}
